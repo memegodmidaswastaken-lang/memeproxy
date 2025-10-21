5fa1dadfed263233075b8f396d5ab6bd4b3665ed
memegodmidaswastaken-lang <memegodmidaswastaken@gmail.com>
2025-10-20 21:18:03 -0500

    Add click event to hide iframe in changeSrc.js

--- js/changeSrc.js
+++ js/changeSrc.js
@@ -1,1 +1,8 @@
+// Placeholder for changeSrc.js
+console.log("changeSrc.js loaded");
+
+// Example: click hider to hide iframe
+document.querySelector('.hider').addEventListener('click', function() {
+    document.querySelector('.frame').style.display = 'none';
+});
 
